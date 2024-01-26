868fcda818ef37bbff30d34bde7356092a29e91c
abbaas1234 <johndoe@example.com>
2024-01-26 10:44:44 +0330

    add routing for elements and collections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ElementsModule } from './elements/elements.module';
+import { CollectionsModule } from './collections/collections.module';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
+    ElementsModule,
+    CollectionsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/elements/elements-routing.module.ts
+++ src/app/elements/elements-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CollectionsHomeComponent } from '../collections/collections-home/collections-home.component';
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'elements', component: ElementsHomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

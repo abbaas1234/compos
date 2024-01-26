26f91fd7825ac8efa8b5784687bd7b818aadc96c
abbaas1234 <johndoe@example.com>
2024-01-26 19:54:50 +0330

    adding table component

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class CollectionsHomeComponent {
 
+  data = [
+    { name: 'ali', age: 24, job: 'designer' },
+    { name: 'reza', age: 21, job: 'engineer' },
+    { name: 'sara', age: 18, job: 'ui' },
+  ];
+
+  header = [
+    { key: 'name', value: 'Name' },
+    { key: 'age', value: 'Age' },
+    { key: 'job', value: 'Job' },
+  ]
+
 }
